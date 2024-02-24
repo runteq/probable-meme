822816ad76780e96e0b69148ed0d74018a2e41fb
Chihiro-Matsuura <146853894+Chihiro-Matsuura@users.noreply.github.com>
2024-02-24 18:59:04 +0900

    Initialize bugfix

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_blog, only: [:create, :destroy]
+
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @comment = @blog.comments.create(comment_params)
+    redirect_to blog_path(@blog)
+  end
+
+  def destroy
+    @comment = @blog.comments.find(params[:id])
+    @comment.destroy!
+    redirect_to blog_path(@blog)
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:name, :content, :blog_id)
+  end
+end
